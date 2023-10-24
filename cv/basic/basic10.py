cd098832c745033581941c169f928ae67663084f
okichi27 <yuliiatryhuk@gmail.com>
2023-10-24 13:26:14 +0300

    updated rep.aboba

--- cv/basic/basic10.py
+++
@@ -1,8 +0,0 @@
-import cv2 
-img=cv2.imread("/home/rodion/yuliia0/aboba/cv/basic/smoothing.jpg")
-cv2.imshow("original",img)
-img = cv2.cvtColor (img, cv2.COLOR_BGR2HSV)
-f = cv2.inRange (img, (50, 30, 50), (255, 255, 255))
-
-cv2.imshow("inRange",f)
-cv2.waitKey (0)
--- cv/trans/trans5.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img=cv2.imread("/home/rodion/yuliia0/aboba/cv/trans/my.jpg")
-
-img =cv2.GaussianBlur(img, (3, 3),0)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-det = cv2.Canny(img, 5, 5*3, 3)
-
-cv2.imshow("2", det)
-cv2.waitKey(0)
